3309d9125269b248c3be8d28a97aefa17eb1a43a
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2024-12-02 17:20:38 +0800

    feat: merge all components for checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "@/components/ui/button";
+import { CrudForm } from "./components/custom_components/CrudForm";
+import { TeamCrudForm } from "./components/custom_components/TeamCrudForm";
+import useFetchMain from "./custom-hooks/useFetchMain";
+import useFetchTeamPlayers from "./custom-hooks/useFetchTeamPlayers";
+import { PointsManager } from "./components/custom_components/PointsManager";
+import { HistoryList } from "./components/custom_components/HistoryList";
+PointsManager;
 
 function App() {
+  const [allData, setAllData] = useState([]);
+  const [teamPlayers, setTeamPlayers] = useState([]);
+  const { mainData } = useFetchMain();
+  const { playersData } = useFetchTeamPlayers();
+
+  useEffect(() => {
+    if (mainData) {
+      setAllData(mainData);
+    }
+  }, [mainData]);
+
+  useEffect(() => {
+    if (playersData) {
+      setTeamPlayers(playersData);
+    }
+  }, [playersData]);
+
   return (
     <div>
-      <Button>Click me</Button>
+      <CrudForm />
+      <TeamCrudForm />
+      <PointsManager />
+      <HistoryList />
+      {allData.length > 0 &&
+        allData.map((data) => (
+          <div key={data.id}>
+            <p className="text-red">{data.full_name}</p>
+            <p className="text-red">{data.team_name}</p>
+          </div>
+        ))}
+
+      {teamPlayers.length > 0 &&
+        teamPlayers.map((player, index) => (
+          <div key={index}>
+            <p className="text-blue">Team Name: {player.team_name}</p>
+            <p className="text-blue">Player Name: {player.player_name}</p>
+          </div>
+        ))}
     </div>
   );
 }
