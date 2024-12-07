66ccc33570db9a606698bbf90509e99a64425e8b
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2024-12-08 01:28:53 +0800

    feat: make the theme dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
 import "./App.css";
-
 import AdminTable from "./components/custom_components/admin/admintable/AdminTable";
 import ProtectedRoute from "@/components/custom_components/ProtectedRoute";
 import MainPage from "@/pages/MainPage";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Login Route */}
-        <Route path="/" element={<MainPage />} />
-
-        {/* Protected Admin Route */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminTable />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          {/* Login Route */}
+          <Route path="/" element={<MainPage />} />
+          {/* Protected Admin Route */}
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminTable />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
