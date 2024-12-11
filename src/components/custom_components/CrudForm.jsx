f8a4a60f6a2636d4724eea4460c7f1f231f74751
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2024-12-12 00:00:21 +0800

    feat: added new games for team and individual

--- src/components/custom_components/CrudForm.jsx
+++ src/components/custom_components/CrudForm.jsx
@@ -161,7 +161,6 @@ export function CrudForm() {
             <TabsTrigger value="team">Team</TabsTrigger>
           </TabsList>
 
-          {/* Player Tab */}
           <TabsContent value="player">
             <div className="flex flex-col gap-4 my-4">
               <Label htmlFor="full_name">Full Name</Label>
@@ -184,15 +183,13 @@ export function CrudForm() {
                   <SelectValue placeholder="Select Section" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectContent>
-                    <SelectItem value="ferrari">Ferrari</SelectItem>
-                    <SelectItem value="aston_martin">Aston Martin</SelectItem>
-                    <SelectItem value="redbull">RedBull</SelectItem>
-                    <SelectItem value="alpine">Alpine</SelectItem>
-                    <SelectItem value="haas">Haas</SelectItem>
-                    <SelectItem value="mercedes">Mercedes</SelectItem>
-                    <SelectItem value="mclaren">McLaren</SelectItem>
-                  </SelectContent>
+                  <SelectItem value="ferrari">Ferrari</SelectItem>
+                  <SelectItem value="aston_martin">Aston Martin</SelectItem>
+                  <SelectItem value="redbull">RedBull</SelectItem>
+                  <SelectItem value="alpine">Alpine</SelectItem>
+                  <SelectItem value="haas">Haas</SelectItem>
+                  <SelectItem value="mercedes">Mercedes</SelectItem>
+                  <SelectItem value="mclaren">McLaren</SelectItem>
                 </SelectContent>
               </Select>
               {errors.section_team && (
@@ -210,40 +207,37 @@ export function CrudForm() {
                   <SelectValue placeholder="Select Game" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="dress_to_impress">
-                    Dress to Impress
+                  <SelectItem value="chess">Chess</SelectItem>
+                  <SelectItem value="rubiks_cube">Rubik's Cube</SelectItem>
+                  <SelectItem value="scrabble">Scrabble</SelectItem>
+                  <SelectItem value="game_of_the_generals">
+                    Game of the Generals
                   </SelectItem>
-
-                  <SelectItem value="chinese_garter">Chinese Garter</SelectItem>
-
-                  <SelectItem value="block_blast">Block Blast</SelectItem>
+                  <SelectItem value="the_clash">The Clash</SelectItem>
                 </SelectContent>
               </Select>
               {errors.game && (
                 <p className="text-red-500 text-sm">{errors.game}</p>
               )}
-              {/* Section Team Select */}
             </div>
             <div className="flex justify-end">
               <Button onClick={handlePlayerSubmit}>Register Player</Button>
             </div>
           </TabsContent>
 
-          {/* Team Tab */}
           <TabsContent value="team">
             <div className="flex flex-col gap-4 my-4">
-              {/* Team Name */}
               <Label htmlFor="team_name">Team Name</Label>
               <Input
                 id="team_name"
                 value={teamData.team_name}
                 onChange={(e) => handleInputChange(e, setTeamData)}
               />
-              {/* Section Team */}
-              <Label htmlFor="section_team">Section Team</Label>
               {errors.team_name && (
                 <p className="text-red-500 text-sm">{errors.team_name}</p>
               )}
+
+              <Label htmlFor="section_team">Section Team</Label>
               <Select
                 onValueChange={(value) =>
                   handleSelectChange("section_team", value, setTeamData)
@@ -265,7 +259,7 @@ export function CrudForm() {
               {errors.section_team && (
                 <p className="text-red-500 text-sm">{errors.section_team}</p>
               )}
-              {/* Game */}
+
               <Label htmlFor="game">Game</Label>
               <Select
                 onValueChange={(value) =>
@@ -276,16 +270,35 @@ export function CrudForm() {
                   <SelectValue placeholder="Select Game" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="flip_cup">Flip Cup</SelectItem>
-
-                  <SelectItem value="chinese_garter">Chinese Garter</SelectItem>
+                  <SelectItem value="mens_basketball">
+                    Men's Basketball
+                  </SelectItem>
+                  <SelectItem value="mens_volleyball">
+                    Men's Volleyball
+                  </SelectItem>
+                  <SelectItem value="badminton_singles">
+                    Badminton Singles
+                  </SelectItem>
+                  <SelectItem value="badminton_doubles">
+                    Badminton Doubles
+                  </SelectItem>
+                  <SelectItem value="valorant">Valorant</SelectItem>
+                  <SelectItem value="mobile_legends">Mobile Legends</SelectItem>
+                  <SelectItem value="league_of_legends">
+                    League of Legends
+                  </SelectItem>
+                  <SelectItem value="general_information_qb">
+                    General Information QB
+                  </SelectItem>
+                  <SelectItem value="general_engineering_qb">
+                    General Engineering QB
+                  </SelectItem>
                 </SelectContent>
               </Select>
               {errors.game && (
                 <p className="text-red-500 text-sm">{errors.game}</p>
               )}
 
-              {/* Players */}
               <Label>Players</Label>
               <ScrollArea>
                 <div className="flex flex-col gap-4 max-h-[100px]">
@@ -318,7 +331,6 @@ export function CrudForm() {
                 Add Player
               </Button>
             </div>
-
             <div className="flex justify-end">
               <Button onClick={handleTeamSubmit}>Register Team</Button>
             </div>
